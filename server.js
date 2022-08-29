e2c88999932eedee0b15b1dfca4a66afa349c5dd
lindat22 <chilinh981992@gmail.com>
2022-08-30 00:39:36 +0700

    ud

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get('/click', async(req,res)=>{
         //res.send(inf)
     
 })
-const PORT = 3000 || process.env.PORT
+const PORT =  process.env.PORT || 3000
 
 app.listen(PORT,()=>{
     console.log(`Server running in port ${PORT}`)
